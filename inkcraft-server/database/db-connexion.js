ec6f434acac63a9b10bb5fc0a753d71bc671c25f
soumia elyakni <selyakni@gmail.com>
2023-02-07 16:28:19 +0100

    connect database

---
+++ inkcraft-server/database/db-connexion.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', true)
+const dbConnect = async () => {
+    const db = process.env.DB_URL
+    try {
+      const data = await mongoose.connect(db);
+  
+      console.log("connected ", data.connection.host);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  module.exports = dbConnect;
