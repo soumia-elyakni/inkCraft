4560a4aa48490b3292e5764034efcb0084ad5f7d
soumia elyakni <selyakni@gmail.com>
2023-02-09 08:57:11 +0100

    create register function

---
+++ inkcraft-server/controllers/users-controller.js
@@ -0,0 +1,68 @@
+const Users = require("../models/Users")
+const Roles = require("../models/Roles")
+const Langues = require("../models/Langues")
+const UsersLangues = require("../models/UsersLangues")
+
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const storage = require("local-storage")
+
+const addLanguesToUser = async(user, langueArray ) => {
+    return new Promise(function(myResolve, myReject) {
+                try{
+                langueArray.forEach(async langue => {
+                    let langueId = await Langues.findOne({name : langue}, '_id')
+
+                    UsersLangues.create({
+                        user_id : user,
+                        langue_id : langueId
+                    })
+                });
+                myResolve();
+            }catch(e){
+                myReject("Problem to affect language")
+            }
+        });
+        
+}
+
+const register = async(req, res) => {
+    const { body } = req 
+
+    if (
+        !body.email ||
+        !body.password ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.cin ||
+        !body.role ||
+        !body.langue
+    ) throw Error("Fill All Inputs")
+
+    const userExist = await Users.findOne({email : body.email })
+    if(userExist) {
+        throw Error("Adresse email already have an account") 
+    } 
+
+    const role = await Roles.findOne({name : body.role})
+    body.role_id = role._id
+
+    const langueArray = await Array.isArray(body.langue) ? body.langue : [body.langue]
+
+    body.password = await bcrypt.hash(body.password, 10)
+
+    const newUser = await Users.create({...body})
+    if (!newUser) throw Error('Regstration failed')
+
+    const newUserId = await Users.findOne({email: newUser.email}, '_id');
+    addLanguesToUser(newUserId, langueArray).then(()=>{
+        res.send('Added Succesfuly')
+    }).catch((error)=>{
+        throw Error(error);
+    })
+    
+
+}
+
+
+module.exports = { register }
