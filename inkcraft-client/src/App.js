c4dfaa65be8d9daa5bc03bd70da4e401685016d4
soumia elyakni <selyakni@gmail.com>
2023-02-26 17:11:38 +0100

    editing App.js routers

--- inkcraft-client/src/App.js
+++ inkcraft-client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
+import './assets/styles/styles.css'
+import { LandingPage } from './pages/public/LandingPage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='*' element="not Found" />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+
+      </Routes>
+    </Router>
   );
 }
 
