fc81f9050a1d4971b2a86ffb127dbe6cca64b053
soumia elyakni <selyakni@gmail.com>
2023-02-26 17:14:19 +0100

    start coding landing page

---
+++ inkcraft-client/src/pages/public/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavBar } from '../../components/build/NavBar'
+
+export const LandingPage = () => {
+  return (
+    <div className='Background'>
+      <NavBar/>
+    </div>
+  )
+}
