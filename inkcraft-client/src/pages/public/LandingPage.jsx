0d3037fdc82d5d737c31c0970da5d14debfe5a47
soumia elyakni <selyakni@gmail.com>
2023-03-11 01:22:17 +0100

    add components to landign pages

--- inkcraft-client/src/pages/public/LandingPage.jsx
+++ inkcraft-client/src/pages/public/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavBar } from '../../components/build/NavBar'
+import { HeroSection } from '../../components/build/home/HeroSection'
 
 export const LandingPage = () => {
   return (
     <div className='Background'>
       <NavBar/>
+      <HeroSection/>
     </div>
   )
 }
