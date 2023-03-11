b22edfb859a0b0572b3d0fd2f1bedc17e73c1a26
soumia elyakni <selyakni@gmail.com>
2023-03-11 01:18:29 +0100

    require login and register components and path

--- inkcraft-client/src/App.js
+++ inkcraft-client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
 import './assets/styles/styles.css'
 import { LandingPage } from './pages/public/LandingPage';
+import {Login} from './pages/auth/Login'
+import { Register } from './pages/auth/Register';
 
 import './App.css';
 
@@ -12,7 +14,8 @@ function App() {
         <Route path='*' element="not Found" />
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<LandingPage />} />
-
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </Router>
   );
