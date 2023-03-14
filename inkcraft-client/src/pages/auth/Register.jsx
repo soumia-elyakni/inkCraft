012ad5b09cf675276e1ec309c725148f012fb186
soumia elyakni <selyakni@gmail.com>
2023-03-14 09:43:21 +0100

    import input button etc component to use it and start connecting with API

--- inkcraft-client/src/pages/auth/Register.jsx
+++ inkcraft-client/src/pages/auth/Register.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-// import { register } from "../../services/auth";
+import { Button } from "../../components/common/Button";
+import { Input } from "../../components/common/Input";
+import { ImageContainer } from "../../components/common/ImageContainer";
+import photo from "../../assets/images/registerpic.png";
+import Checkbox from "../../components/common/Checkbox";
+import { register } from "../../services/auth-services";
 
 export const Register = () => {
   const [newUser, setNewUser] = useState({
@@ -37,185 +42,154 @@ export const Register = () => {
     setNewUser({ ...newUser, role: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   console
+  //     .log(newUser)
+  //     // await register({...newUser})
+  //     .then((response) => {
+  //       alert(response.data);
+  //     })
+  //     .catch((error) => {
+  //       alert(error.response.data.message);
+  //     });
+  // };
+
+  const handleSubmit = async (e) =>{
     e.preventDefault();
     console.log(newUser)
-    // await register({...newUser})
-    .then(response => {
-      alert(response.data)
-  })
-  .catch(error => {
-    alert(error.response.data.message);
-  });
+    await register(newUser)
+    .then((response)=>{
+      alert(response.data);
+    })
+    .catch((error)=>{
+      alert(error.response.data.message)
+    })
   }
 
   return (
-    <div className="flex mt-6 items-center justify-center">
-      <div className="flex flex-col  max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-        <div className="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl dark:text-white">
-          Create a new account
-        </div>
-        <span className="justify-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
-          Already have an account ?
-          <Link
-            to="/login"
-            className="text-sm text-blue-500 underline hover:text-blue-700"
-          >
-            Sign in
-          </Link>
-        </span>
+    <div className="flex items-center justify-center w-full">
+      <div className="flex flex-col w-full h-full my-5 mx-5  bg-white rounded-lg shadow">
+        <div className="flex flex-row justify-between form-container w-full">
 
-        <div className="p-6 mt-8">
-          <form  onSubmit={handleSubmit}>
-            <div className="flex flex-col mb-2">
-              <div className=" relative ">
-                <input
-                  type="email"
-                  name="email"
-                  id="create-account-email"
-                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  value={newUser.email}
-                  onChange={handleChange}
-                  placeholder="Email"
-                />
+        <div className="picture-container flex flex-col w-1/2 h-full ">
+            <ImageContainer className="h-full rounded-l-lg shadow-picture" src={photo} />
+          </div>
+          <div className="flex flex-col overflow-y-auto scroll-auto form-container w-1/2 pt-20 items-center justify-center m-auto">
+            
+              <div className="  flex flex-col items-center pt-20 m-6">
+                <div className="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl dark:text-white">
+                  Create a new account
+                </div>
               </div>
-            </div>
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-col mb-2">
+                  <div className=" relative ">
+                    <Input
+                      type="email"
+                      name="email"
+                      id="create-account-email"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      value={newUser.email}
+                      onChange={handleChange}
+                      placeholder="Email"
+                    />
+                  </div>
+                </div>
 
-            <div className="flex gap-4 mb-2">
-              <div className=" relative ">
-                <input
-                  type="text"
-                  id="create-account-first-name"
-                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  name="first_name"
-                  placeholder="First name"
-                  value={newUser.first_name}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className=" relative ">
-                <input
-                  type="text"
-                  id="create-account-last-name"
-                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  name="last_name"
-                  placeholder="Last name"
-                  value={newUser.last_name}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
+                <div className="flex gap-4 mb-2">
+                  <div className=" relative ">
+                    <Input
+                      type="text"
+                      id="create-account-first-name"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      name="first_name"
+                      placeholder="First name"
+                      value={newUser.first_name}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className=" relative ">
+                    <Input
+                      type="text"
+                      id="create-account-last-name"
+                      className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      name="last_name"
+                      placeholder="Last name"
+                      value={newUser.last_name}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
 
-            <div className="flex flex-col mb-2">
-              <div className=" relative ">
-                <input
-                  type="text"
-                  id="create-account-cin"
-                  className=" rounded-lg border-transparent appearance-none flex-1  border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  name="cin"
-                  placeholder="Your CIN"
-                  value={newUser.cin}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
+                <div className="flex flex-col mb-2">
+                  <div className=" relative ">
+                    <Input
+                      type="text"
+                      id="create-account-cin"
+                      className=" rounded-lg border-transparent appearance-none flex-1  border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      name="cin"
+                      placeholder="Your CIN"
+                      value={newUser.cin}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
 
+                <div className="flex flex-col mb-2">
+                  <div className=" relative ">
+                    <Input
+                      type="password"
+                      id="create-account-password"
+                      className=" rounded-lg border-transparent appearance-none flex-1  border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      name="password"
+                      placeholder="Password"
+                      value={newUser.pass}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
 
-            <div className="flex flex-col mb-2">
-              <div className=" relative ">
-                <input
-                  type="password"
-                  id="create-account-password"
-                  className=" rounded-lg border-transparent appearance-none flex-1  border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  name="password"
-                  placeholder="Password"
-                  value={newUser.pass}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-start mt-6">
-              <div>
-                <h6 className="mb-4">Choose your work languages</h6>
-                <label className="flex items-center mb-3 space-x-3">
-                  <input
+                <div className="flex items-center justify-start mt-3">
+                  <Checkbox
+                    title="Choose your work languages"
                     type="checkbox"
-                    name="frensh"
-                    value="frensh"
+                    name = "langues"
+                    items={["French", "English", "Arabic"]}
                     onChange={handleLanguageChange}
-                    className="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500 checked:border-transparent focus:outline-none"
                   />
-                  <span className="font-normal text-gray-700 dark:text-white">
-                    Frensh
-                  </span>
-                </label>
-                <label className="flex items-center mb-3 space-x-3">
-                  <input
-                    type="checkbox"
-                    name="english"
-                    value="english"
-                    onChange={handleLanguageChange}
-                    className="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500 checked:border-transparent focus:outline-none"
-                  />
-                  <span className="font-normal text-gray-700 dark:text-white">
-                    English
-                  </span>
-                </label>
-                <label className="flex items-center mb-3 space-x-3">
-                  <input
-                    type="checkbox"
-                    name="arabe"
-                    value="arabe"
-                    onChange={handleLanguageChange}
-                    className="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500 checked:border-transparent focus:outline-none"
-                  />
-                  <span className="font-normal text-gray-700 dark:text-white">
-                    Arabic
-                  </span>
-                </label>
-              </div>
-            </div>
+                </div>
 
-            <div className="flex items-center justify-start mt-6">
-              <div>
-                <h6 className="mb-4">Choose your role</h6>
-                <label className="flex items-center mb-3 space-x-3">
-                  <input
+                <div className="flex items-center justify-start mt-3">
+                  <Checkbox
+                    title="Choose your role"
                     type="radio"
+                    items={["Writer", "Translator"]}
                     name="role"
-                    value="translator"
                     onChange={handleRoleChange}
-                    className="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500 checked:border-transparent focus:outline-none"
                   />
-                  <span className="font-normal text-gray-700 dark:text-white">
-                    translator
-                  </span>
-                </label>
-                <label className="flex items-center mb-3 space-x-3">
-                  <input
-                    type="radio"
-                    name="role"
-                    value="writer"
-                    onChange={handleRoleChange}
-                    className="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500 checked:border-transparent focus:outline-none"
-                  />
-                  <span className="font-normal text-gray-700 dark:text-white">
-                    writer
-                  </span>
-                </label>
-              </div>
-            </div>
+                </div>
 
-            <div className="flex w-full my-4">
-              <button
-                type="submit"
-                className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-              >
-                Register
-              </button>
-            </div>
-          </form>
+                <div className="flex w-full my-4">
+                  <Button
+                    type="submit"
+                    className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    text="Register"
+                  />
+                </div>
+              </form>
+              <span className="justify-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400 mb-8">
+                  Already have an account ?
+                  <Link
+                    to="/login"
+                    className="text-sm text-blue-500 underline hover:text-blue-700"
+                  >
+                    Sign in
+                  </Link>
+                </span>
+            
+          </div>
+
         </div>
       </div>
     </div>
