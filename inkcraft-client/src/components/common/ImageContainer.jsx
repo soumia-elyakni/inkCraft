f60851d2cb270d0b1aa704569261487143fc8362
soumia elyakni <selyakni@gmail.com>
2023-03-14 09:39:17 +0100

    adjust imageContainer className props

--- inkcraft-client/src/components/common/ImageContainer.jsx
+++ inkcraft-client/src/components/common/ImageContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ImageContainer = (props) => {
   return (
-    <img  src={props.src} alt={props.alt}  className={`picture shadow-picture + ${props.className}`}/>
+    <img  src={props.src} alt={props.alt}  className={`picture  + ${props.className}`}/>
   );
 };
 
