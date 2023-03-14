199880a427783a8609564bcbdad4b0fc2c689aad
soumia elyakni <selyakni@gmail.com>
2023-03-14 09:38:34 +0100

    create checkbox component

---
+++ inkcraft-client/src/components/common/Checkbox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Checkbox = ({title, type, items, onChange, name}) => {
+  return (
+    <div className="flex items-center justify-start mt-6">
+      <div>
+        <h6 className="mb-4">{title}</h6>
+        {items.map((item) => (
+          <label key={item} className="flex items-center mb-3 space-x-3">
+            <input
+              type={type}
+              name={type === "radio" ? name : item}
+              value={item}
+              onChange={onChange}
+              className="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500 checked:border-transparent focus:outline-none"
+            />
+            <span className="font-normal text-gray-700 dark:text-white">{item}</span>
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+Checkbox.propTypes = {
+  title: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["checkbox", "radio"]).isRequired,
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Checkbox;
