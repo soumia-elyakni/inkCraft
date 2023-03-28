15aa80261291769b9932e2a3174fcf2bee96623a
soumia elyakni <selyakni@gmail.com>
2023-03-28 23:24:51 +0000

    create storie liste component file

---
+++ inkcraft-client/src/pages/writter/outlet/StoriesListe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StorieCard } from '../../../components/build/writter/StorieCard'
+
+export const StoriesListe = () => {
+  return (
+    <div>
+        <StorieCard/>
+        <StorieCard/>
+    </div>
+  )
+}
