d1f307ef3d9bb97ec23b9b075a5bb050f38f8d30
soumia elyakni <selyakni@gmail.com>
2023-03-28 15:49:13 +0000

    add background to landing page

--- inkcraft-client/src/pages/public/LandingPage.jsx
+++ inkcraft-client/src/pages/public/LandingPage.jsx
@@ -4,9 +4,12 @@ import { HeroSection } from '../../components/build/home/HeroSection'
 
 export const LandingPage = () => {
   return (
-    <div className='Background'>
+    <div className=''>
       <NavBar/>
+      <div>
+      <img src="https://images.pexels.com/photos/1425146/pexels-photo-1425146.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="Background absolute object-cover w-full h-full"/>
       <HeroSection/>
+      </div>
     </div>
   )
 }
