e3f22189580f823621d5e8a68635a447a8205ff4
soumia elyakni <selyakni@gmail.com>
2023-04-05 15:39:03 +0000

    Add props to storieCard

--- inkcraft-client/src/components/build/writter/StorieCard.jsx
+++ inkcraft-client/src/components/build/writter/StorieCard.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import {  useNavigate } from "react-router-dom";
 
-const StorieCard = () => {
-  const [content, setContent] = useState(
-    "L'histoire d'une fille adoptée par une femme qui s'appelle Hadeel. Hadeel a passé une grave période à l'hôpital. Sibel a fait un don de cœur pour Hadeel."
-  );
+const StorieCard = ({ id, title, description, langue, resume, personnages }) => {
+  const navigate = useNavigate()
+  const [showDetails, setShowDetails] = useState(false);
   const [showContent, setShowContent] = useState(false);
 
   function toggleContent() {
     setShowContent(!showContent);
   }
+  const [content, setContent] = useState(
+    description
+  );
+
+  function toggleDetails() {
+    setShowDetails(!showDetails);
+  }
 
   return (
-    <div className="">
+    <div className="" style={{ width: 300 }}>
       <div className="relative w-full px-4 py-6 bg-white shadow-lg dark:bg-gray-700 rounded-2xl">
         <h3 className="text-2xl font-bold text-black dark:text-white">
-          Sibel
+          {title}
         </h3>
         <p className="text-sm inline-block text-gray-400">
-          {showContent ? content : content.slice(0, 100) + '...'}
-          {!showContent ? (
+          {showContent ? content : content.slice(0, 70) + '...'}
+        </p>
+        {!showContent ? (
             <button onClick={toggleContent}>Voir plus</button>
           ) : (
             <button onClick={toggleContent}>Voir moins</button>
           )}
-        </p>
-        <p className="text-sm text-gray-400">Langue : Français</p>
+      
+        <p className="text-sm text-gray-400">Langue :{langue}</p>
+        <button className="mr-2"
+        onClick={()=>{
+            navigate(`${id}` , {state : {id: id, title: title, description: description, resume: resume, langue: langue, personnages: personnages}})
+        }}>
+        Voir en détail
+</button>
+
         <button
           className="mr-2"
           onClick={() =>
-            alert('Vous devez vous connecter pour voir les détails')
-          }
-        >
-          Voir en privé
-        </button>
-        <button
-          className="mr-2"
-          onClick={() =>
-            alert('Vous devez vous connecter pour modifier cette histoire')
+            alert("Vous devez vous connecter pour modifier cette histoire")
           }
         >
           Modifier
@@ -46,4 +53,4 @@ const StorieCard = () => {
   );
 };
 
-export  {StorieCard};
+export { StorieCard };
