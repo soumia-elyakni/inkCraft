dda8b112c4eefd6ef777df874867273f2f474ed5
soumia elyakni <selyakni@gmail.com>
2023-04-05 15:37:27 +0000

    Add and declare Storie details component path

--- inkcraft-client/src/App.js
+++ inkcraft-client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router , Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './assets/styles/styles.css'
 import { LandingPage } from './pages/public/LandingPage';
-import {Login} from './pages/auth/Login'
+import { Login } from './pages/auth/Login'
 import { Register } from './pages/auth/Register';
 import { WritterDashboard } from './pages/writter/WritterDashboard';
 import { StoriesListe } from './pages/writter/outlet/StoriesListe';
 import { TranslatorDashboard } from './pages/translator/TranslatorDashboard';
 
 import './App.css';
+import { StorieDetails } from './pages/writter/outlet/StorieDetails';
 
 function App() {
   return (
@@ -17,18 +18,19 @@ function App() {
         <Route path='*' element="not Found" />
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<LandingPage />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/dash" element={<WritterDashboard/>} >
-          <Route path="stories" element={<StoriesListe/>}/>
-          <Route path="persons" element={<LandingPage></LandingPage>}/>
-          <Route path="places" element="{<places/>}"/>
-          <Route path="events" element="{<events/>}"/>
-          <Route path="chapters" element="{<chapters/>}"/>
-          <Route path="tools" element="{<tools/>}"/>
-          <Route path="settings" element="{<settings/>}"/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dash" element={<WritterDashboard />} >
+          <Route path="stories" element={<StoriesListe />} />
+          <Route path="stories/:id" element={<StorieDetails />} />
+          <Route path="persons" element="{<persons/>}" />
+          <Route path="places" element="{<places/>}" />
+          <Route path="events" element="{<events/>}" />
+          <Route path="chapters" element="{<chapters/>}" />
+          <Route path="tools" element="{<tools/>}" />
+          <Route path="settings" element="{<settings/>}" />
         </Route>
-        <Route path="/tdash" element={<TranslatorDashboard/>} />
+        <Route path="/tdash" element={<TranslatorDashboard />} />
       </Routes>
     </Router>
   );
