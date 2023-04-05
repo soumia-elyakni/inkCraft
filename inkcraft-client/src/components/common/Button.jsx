f48610d257179f706d1568b550935600e63cb7c9
soumia elyakni <selyakni@gmail.com>
2023-04-05 15:39:40 +0000

    Add OnClick props to Button

--- inkcraft-client/src/components/common/Button.jsx
+++ inkcraft-client/src/components/common/Button.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export const Button = (props) => {
   return (
-    <button className={`button button-${props.className}  transition-colors duration-300`}>
-      {props.text}
+    <button className={`button button-${props.className}  transition-colors duration-300`} onClick={props.onClick}>
+      {props.text} 
     </button>
   );
 }
