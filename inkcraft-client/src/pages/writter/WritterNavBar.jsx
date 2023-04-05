a971cc8278bae7bcba402ccc63206da77e73fe04
soumia elyakni <selyakni@gmail.com>
2023-04-05 15:52:14 +0000

    Add logout button

--- inkcraft-client/src/pages/writter/WritterNavBar.jsx
+++ inkcraft-client/src/pages/writter/WritterNavBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { FaUserCircle } from "react-icons/fa";
 import Logo from '../../assets/images/logo.png'
+import { Button } from '../../components/common/Button';
+import { logout } from '../../services/auth-services';
 
 export const WritterNavBar = () => {
     const user = JSON.parse(localStorage.getItem("user"))
+    
+    const navigation = useNavigate()
+
+    const handleLogout = async () => {
+      try {
+        await logout()
+        navigation('/login')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
   return (
     <nav className="bg-opacity-30 py-4">
     <div className="max-w-7xl mx-auto px-4">
@@ -25,6 +39,9 @@ export const WritterNavBar = () => {
         <div className="flex items-center space-x-4">
           <FaUserCircle className="text-green-200 text-2xl" />
           <div>{user.name}</div>
+          <div>
+            <Button className=" bg-lime-300 text-xl" text="Out" onClick={handleLogout} />
+          </div>
         </div>
       </div>
     </div>
