7ce115a7a66c64b3979fc404260ff42aacb0b462
soumia elyakni <selyakni@gmail.com>
2023-04-07 11:15:28 +0000

    add authenticate in routes

--- inkcraft-server/routers/writter-routers.js
+++ inkcraft-server/routers/writter-routers.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 
 const { addStorie, getStoriesByUserId } = require('../controllers/stories-controller')
+const { authenticate } = require('../middlewares/auth')
 const { tryCatch } = require('../middlewares/try-catch')
 
 
 
-router.post('/addStorie', tryCatch(addStorie))
-router.post('/getAllStories', tryCatch(getStoriesByUserId))
+router.post('/addStorie', authenticate, tryCatch(addStorie))
+router.get('/getAllStories', authenticate, tryCatch(getStoriesByUserId))
 
 module.exports = router
