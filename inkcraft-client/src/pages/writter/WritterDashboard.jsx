a27893bfda7ac4da25727471fd68179781e2e5a0
soumia elyakni <selyakni@gmail.com>
2023-04-12 18:54:24 +0000

    add a third div

--- inkcraft-client/src/pages/writter/WritterDashboard.jsx
+++ inkcraft-client/src/pages/writter/WritterDashboard.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { WritterSidebar } from './WritterSidebar'
-import { WritterNavBar } from './WritterNavBar'
 import { Outlet } from 'react-router'
+import { NavBar } from '../../components/build/NavBar'
+import img from "../../assets/images/back.jpg"
 
 export const WritterDashboard = () => {
 
   return (
     <div>
-    <WritterNavBar/>
-    <div className='fixed flex flex-row'>
-    <WritterSidebar/>
-    <div className="inline-block ml-3 border max-h-full border-green-900">
-  <Outlet />
-</div>
+      <img src={img} alt='backgroundImg' className="Background fixed top-0 left-0 object-cover w-full h-full"/>
+      <div className='mb-10'>
+        <NavBar />
+      </div>
+      <div className='flex flex-row my-20 pt-5'>
+        <div className=' w-1/4 fixed'>
+          <WritterSidebar />
+        </div>
+        <div className="w-1/2 fixed pl-3 max-h-screen overflow-y-hidden scrollbar-none" style={{ marginLeft: 350, maxHeight: '85%' , overflowY: 'scroll' }}>
+          <Outlet />
+        </div>
+        <div className="w-1/4" />
+      </div>
     </div>
-    </div>
-  )
+  );
+
 }
