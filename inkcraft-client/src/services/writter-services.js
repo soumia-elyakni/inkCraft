28d1b4a0479b0327efb9570fb3ebbc7c92995c11
soumia elyakni <selyakni@gmail.com>
2023-04-13 04:13:45 +0000

    addStorie Service

--- inkcraft-client/src/services/writter-services.js
+++ inkcraft-client/src/services/writter-services.js
@@ -4,27 +4,46 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5500/api/writter/";
 
-const addStorie = async({storie}) => {
+const addStorie = async ( storie ) => {
+  console.log(storie)
+  const token = localStorage.getItem("token");
 
+  try {
+    const response = await axios.post(
+      API_URL + "addStorie",
+      storie ,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 const getAllStories = async () => {
   const token = localStorage.getItem('token');
-  
-  
-  const response = await axios.get(API_URL + 'getAllStories', { headers: {
-    Authorization: `Bearer ${token}` 
-  }});
 
-  console.log('Arbi makhdm')
-  console.log(response.data)
+
+  const response = await axios.get(API_URL + 'getAllStories', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+ 
 
   return response.data;
 };
 
-  
 
-  export {
-    getAllStories
-  }
-  
+
+export {
+  addStorie,
+  getAllStories
+}
