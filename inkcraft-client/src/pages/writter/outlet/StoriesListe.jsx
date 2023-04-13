d60cecd71cb9b0a19c1f2561537f169a28c8df52
soumia elyakni <selyakni@gmail.com>
2023-04-13 03:32:21 +0000

    add create button

--- inkcraft-client/src/pages/writter/outlet/StoriesListe.jsx
+++ inkcraft-client/src/pages/writter/outlet/StoriesListe.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
-// import { GiSilverBullet } from 'react-icons/gi'
+import { GiSilverBullet } from 'react-icons/gi'
 import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import { StorieCard } from '../../../components/build/writter/StorieCard'
 import { getAllStories } from '../../../services/writter-services';
+import { Button } from '../../../components/common/Button';
 
 export const StoriesListe = () => {
   const [stories, setStories] = useState([])
+  const navigate = useNavigate()
+  const activeStorie = localStorage.getItem("activeStorie")
 
   useEffect(() => {
     getAllStories()
@@ -16,25 +19,35 @@ export const StoriesListe = () => {
     })
     .catch((err) => console.log(err));
   },[]);
+
+ 
+  
   
 
 return (
-  <div className="grid grid-cols-2 gap-6 p-2 mt-3 py-4">
-  {stories && stories.length > 0 ? (
-    stories.map((storie) => (
-      <StorieCard
-        key={storie._id}
-        id={storie._id}
-        title={storie.title}
-        description={storie.description}
-        langue={storie.langue}
-        resume={storie.resume}
-        personnages={storie.personnages}
-      />
-    ))
-  ) : (
-    <p>No stories found.</p>
-  )}
+  <div className="grid grid-cols-2 gap-6 p-2 mt-3 py-4 bg-slate-50">
+      <div className='fixed right-0 bg-lime-800 rounded-3xl'>
+          <Button text={<GiSilverBullet/>} onClick={()=> navigate('add')} />
+      </div>
+      {stories && stories.length > 0 ? (
+        stories.map((storie) => {
+          const isStorieActive = activeStorie === storie._id;
+          return (
+            <StorieCard
+              key={storie._id}
+              id={storie._id}
+              title={storie.title}
+              description={storie.description}
+              langue={storie.langue}
+              resume={storie.resume}
+              personnages={storie.personnages}
+              isActived={isStorieActive}
+            />
+          );
+        })
+      ) : (
+        <p>No stories found.</p>
+      )}
 </div>
 );
     
